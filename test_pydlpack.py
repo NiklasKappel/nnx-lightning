b5e57d5025becb7a250aea3d05105b0c42d777c5
Niklas Kappel <niklas.kappel@kit.edu>
2025-01-21 15:20:02 +0100

    Update

--- test_pydlpack.py
+++ test_pydlpack.py
@@ -8,7 +8,6 @@ jax.config.update("jax_default_device", jax.devices()[-1])
 
 import torch
 
-assert torch.cuda.is_available()
 torch.set_default_device(f"cuda:{torch.cuda.device_count() - 1}")
 
 import dlpack.tests
