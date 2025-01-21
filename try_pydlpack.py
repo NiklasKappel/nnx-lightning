671cc453258379727a09f0b01564fb13eec0529b
Niklas Kappel <niklas.kappel@kit.edu>
2025-01-21 14:05:42 +0100

    Update

--- try_pydlpack.py
+++ try_pydlpack.py
@@ -1,15 +1,20 @@
 import jax
+
+jax.config.update("jax_default_device", jax.devices()[-1])
+
 import torch
-from dlpack import asdlpack
 
 assert torch.cuda.is_available()
+torch.set_default_device(f"cuda:{torch.cuda.device_count() - 1}")
+
+from dlpack import asdlpack
 
 a1 = jax.numpy.array([[1, 2], [3, 4]])
 t1 = torch.from_dlpack(asdlpack(a1))
 print(a1, a1.device)
-print(t1)
+print(t1, t1.device)
 
 t2 = torch.tensor([[5, 6], [7, 8]]).cuda()
 a2 = jax.numpy.from_dlpack(asdlpack(t2))
-print(t2)
+print(t2, t2.device)
 print(a2, a2.device)
