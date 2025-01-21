8f83a3146dcc9a7f737bb3703786514ced2f47da
Niklas Kappel <niklas.kappel@kit.edu>
2025-01-21 14:15:44 +0100

    Update

--- try_pydlpack.py
+++ try_pydlpack.py
@@ -9,12 +9,16 @@ torch.set_default_device(f"cuda:{torch.cuda.device_count() - 1}")
 
 from dlpack import asdlpack
 
-a1 = jax.numpy.array([[1, 2], [3, 4]])
-t1 = torch.from_dlpack(asdlpack(a1))
-print(a1, a1.device)
-print(t1, t1.device)
+initial_jax_array = jax.random.normal(jax.random.PRNGKey(0), (1024, 1024))
+initial_torch_tensor = torch.randn(1024, 1024)
+print(initial_jax_array.device, initial_torch_tensor.device)
 
-t2 = torch.tensor([[5, 6], [7, 8]]).cuda()
-a2 = jax.numpy.from_dlpack(asdlpack(t2))
-print(t2, t2.device)
-print(a2, a2.device)
+# a1 = jax.numpy.array([[1, 2], [3, 4]])
+# t1 = torch.from_dlpack(asdlpack(a1))
+# print(a1, a1.device)
+# print(t1, t1.device)
+#
+# t2 = torch.tensor([[5, 6], [7, 8]]).cuda()
+# a2 = jax.numpy.from_dlpack(asdlpack(t2))
+# print(t2, t2.device)
+# print(a2, a2.device)
